664fcef6f0813605fdf81212fe7c42950ba3d9a4
Freivin Campbell <freivin12@gmail.com>
2021-11-27 06:14:54 -0600

    implementing short code generating

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -5,8 +5,19 @@ class ShortUrl < ApplicationRecord
 
   validate :validate_full_url
 
+  before_create :short_code
+
   scope :top_short_url, ->(limit) { order(click_count: :desc).limit(limit).as_json(except: %i[created_at updated_at]) }
+
   def short_code
+    self.shorted_code = ShortUrlsHelper.url_encode(next_id)
+  end
+
+  def next_id
+    last_id = ShortUrl.last&.id
+    return 1 if last_id.nil?
+
+    last_id + 1
   end
 
   def update_title!
