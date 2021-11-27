3c8b15754fd994b19315cfff907937ae13383f7b
Freivin Campbell <freivin12@gmail.com>
2021-11-27 06:16:30 -0600

    new helper to encode and decode short codes

--- app/helpers/short_urls_helper.rb
+++ app/helpers/short_urls_helper.rb
@@ -1,2 +1,24 @@
 module ShortUrlsHelper
+  CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
+
+  def self.url_encode(idx)
+    return CHARACTERS[0] if idx.zero?
+
+    str = ''
+    base = CHARACTERS.length
+
+    while idx.positive?
+      str << CHARACTERS[idx.modulo(base)]
+      idx /= base
+    end
+
+    str.reverse
+  end
+
+  def self.url_decode(str)
+    i = 0
+    base = CHARACTERS.length
+    str.each_char { |c| i = i * base + CHARACTERS.index(c) }
+    i
+  end
 end
