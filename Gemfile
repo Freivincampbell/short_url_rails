2b2bd3e5b71fb99bf16545d9c6ef5590e86253fd
Freivin Campbell <freivin12@gmail.com>
2021-11-29 20:40:35 -0600

    Adding logic to update a title under background / using http party gem

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.7'
 gem "json"
 gem "resque"
 gem 'rack-cors'
+gem 'httparty', '~> 0.20.0'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/jobs/update_title_job.rb
+++ app/jobs/update_title_job.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal true
+
+# Job to update the title of a resource
 class UpdateTitleJob < ApplicationJob
   queue_as :default
 
   def perform(short_url_id)
+    if (url = ShortUrl.find(short_url_id))
+      response = HTTParty.get(url.full_url)
+      html = Nokogiri::HTML.parse(response.body)
 
+      url.update(title: html.title)
+    end
   end
 end
